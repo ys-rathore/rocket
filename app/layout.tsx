0b840587b3130db203108ed5768491e7e6b8f406
Yuvraj Singh <112612239+ys-rathore@users.noreply.github.com>
2025-10-30 14:15:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'YSpace',
+  description: 'Lets Creat it !',
+  generator: 'Space.in',
 }
 
 export default function RootLayout({
